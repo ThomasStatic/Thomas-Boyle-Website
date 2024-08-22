a7543fa78417397aee684554ade3366bf1e16e3a
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-08-22 19:00:11 -0400

    ported over old code to new repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UsingThisSiteComponent } from './home-page/using-this-site/using-this-site.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent, HomePageComponent, UsingThisSiteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'thomas-boyle-website';
+  title = 'personalProject';
 }
