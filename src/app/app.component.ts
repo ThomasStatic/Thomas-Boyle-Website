e7d811dd08b10d5bba0d9404a23a8f778b18dda2
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-09-01 17:59:46 -0400

    Got rid of clutter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { UsingThisSiteComponent } from './home-page/using-this-site/using-this-site.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent, HomePageComponent, UsingThisSiteComponent],
+  imports: [RouterOutlet, NavigationBarComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
