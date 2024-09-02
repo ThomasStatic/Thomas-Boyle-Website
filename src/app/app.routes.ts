4a873344b36c9bd475cc3423050ffe78edaf3fa5
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-09-02 00:45:48 -0400

    set up routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './home-page/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
