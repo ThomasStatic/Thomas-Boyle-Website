9a47bc97d6e87082f97d2e0d606282b2aac8656f
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-09-02 13:31:33 -0400

    added strong visualizer to nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './home-page/header/header.component';
+import { StrongVisualizerComponent } from './strong-visualizer/strong-visualizer.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
+    { path: 'strong-visualizer', component: StrongVisualizerComponent},
     { path: '**', redirectTo: '' }
 ];
 
---
+++ src/app/strong-visualizer/strong-visualizer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strong-visualizer',
+  standalone: true,
+  imports: [],
+  templateUrl: './strong-visualizer.component.html',
+  styleUrl: './strong-visualizer.component.scss'
+})
+export class StrongVisualizerComponent {
+
+}
