6f6f7efe0e68f3939c20a4c633786c6ab23093d3
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-09-02 22:22:48 -0400

    created inputs for data filters component

--- src/app/strong-visualizer/data-filters/data-filters.component.ts
+++ src/app/strong-visualizer/data-filters/data-filters.component.ts
@@ -1,18 +1,44 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
-// import { MatRad }
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-data-filters',
   standalone: true,
-  imports: [MatCardModule, MatSelectModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatCardModule, 
+    MatSelectModule, 
+    MatRadioModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatButtonModule,
+  ],
   templateUrl: './data-filters.component.html',
   styleUrl: './data-filters.component.scss'
 })
 export class DataFiltersComponent {
 
   csvContent = input('');
+
+  selectedRadioButton = '1RM';
+
+  readonly metricFormControl = new FormControl();
+  readonly exerciseFormControl = new FormControl();
+  readonly options = inject(FormBuilder).group({
+    exercises: this.exerciseFormControl,
+    metric: this.metricFormControl,
+  });
 
   
 
