5b82cdfd81a4d1ea4234c21a6488cf5da0998eca
Thomas Boyle <tsboyle10@gmail.com>
2025-08-16 19:59:38 -0400

    generating random card image

--- src/app/blackjack/blackjack.component.ts
+++ src/app/blackjack/blackjack.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CardName, Deck, PlayingCard, Suit } from 'typedeck';
+import { Card, CardName, Deck, ICard, PlayingCard, Suit } from 'typedeck';
 
 @Component({
   selector: 'app-blackjack',
@@ -13,11 +13,16 @@ export class BlackjackComponent implements OnInit {
   validCardNumbers: CardName[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   validCardSuits: Suit[] = [0, 1, 2, 3]; // Suit is an enum with values 0-3
   deck: Deck | null = null;
-  cards: PlayingCard[] = [];
+  cards: ICard[] = [];
+
+  playersHand: PlayingCard[] = [];
 
   ngOnInit(): void {
     this.refreshDeck();
     this.deck?.shuffle();
+    const card = this.deck?.takeCard();
+    this.playersHand.push(card as PlayingCard);
+    console.log('Player\'s hand:', this.playersHand);
   } 
   
   refreshDeck(): void {
@@ -27,6 +32,56 @@ export class BlackjackComponent implements OnInit {
       }
     }
     this.deck = new Deck(this.cards);
+  }
+
+  protected getImagePath(card: PlayingCard): string {
+    let cardNum = this.getCardNum(card.cardName);
+    let cardSuit = this.getCardSuit(card.suit);
+    return `playing-cards/${cardNum}_of_${cardSuit}.png`;    
+  }
+
+  private getCardNum(cardNum: number): string {
+    switch(cardNum) {
+      case CardName.Ace:
+        return "ace";
+      case CardName.Two:
+        return "two";
+      case CardName.Three:
+        return "three";
+      case CardName.Four:
+        return "four";
+      case CardName.Five:
+        return "five";
+      case CardName.Six:
+        return "six";
+      case CardName.Seven:
+        return "seven";
+      case CardName.Eight:
+        return "eight";
+      case CardName.Nine:
+        return "nine";
+      case CardName.Ten:
+        return "ten";
+      case CardName.Jack:
+        return "jack";
+      case CardName.Queen:
+        return "queen";
+      default:
+        return "king";
+    }
+  }
+
+  private getCardSuit(cardSuit: number): string {
+    switch(cardSuit) {
+      case Suit.Clubs:
+        return "ace";
+      case Suit.Diamonds:
+        return "diamonds";
+      case Suit.Hearts:
+        return "hearts";
+      default:
+        return "spades";
+    }
   }
 
 }
