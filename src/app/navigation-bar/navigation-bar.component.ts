31889ad46acba65b58154e8184bbc9c3392b95ea
Thomas Boyle <tsboyle10@gmail.com>
2026-06-27 23:37:50 -0400

    adding refresh logic to navbar tabs for a page you are already on

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
-  
+  constructor(
+    private readonly router: Router,
+    @Inject(DOCUMENT) private readonly document: Document
+  ) {}
+
+  refreshIfCurrentRoute(event: MouseEvent, route: string): void {
+    if (
+      event.defaultPrevented ||
+      event.button !== 0 ||
+      event.altKey ||
+      event.ctrlKey ||
+      event.metaKey ||
+      event.shiftKey
+    ) {
+      return;
+    }
+
+    if (this.normalizePath(this.router.url) !== this.normalizePath(route)) {
+      return;
+    }
+
+    event.preventDefault();
+    this.document.defaultView?.location.reload();
+  }
+
+  private normalizePath(path: string): string {
+    const pathOnly = path.split(/[?#]/)[0] || '/';
+    const pathWithLeadingSlash = pathOnly.startsWith('/') ? pathOnly : `/${pathOnly}`;
+
+    return pathWithLeadingSlash.length > 1
+      ? pathWithLeadingSlash.replace(/\/+$/, '')
+      : '/';
+  }
 }
